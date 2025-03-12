6b366b6839c5ea54dc4bb52e872c98d18eb7f4af
yashjain63 <105098861+yashjain63@users.noreply.github.com>
2025-03-12 16:58:50 +0530

    Update AppContext.jsx

--- admin/src/context/AppContext.jsx
+++ admin/src/context/AppContext.jsx
@@ -6,7 +6,7 @@ export const AppContext = createContext()
 const AppContextProvider = (props) => {
 
     const currency = import.meta.env.VITE_CURRENCY
-    const backendUrl = "https://doctorsappointmentbackend.onrender.com"
+    const backendUrl = import.meta.env.VITE_BACKEND_URL
 
     const months = ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"]
 
