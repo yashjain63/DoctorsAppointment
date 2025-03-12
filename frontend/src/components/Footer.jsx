9295d743e1b062b35d8c407743f4765c5e0976f9
yashjain <yjain1049@gmail.com>
2025-03-12 12:38:17 +0530

    initial commit

---
+++ frontend/src/components/Footer.jsx
@@ -0,0 +1,44 @@
+import React from 'react'
+import { assets } from '../assets/assets'
+
+const Footer = () => {
+  return (
+    <div className='md:mx-10'>
+      <div className='flex flex-col sm:grid grid-cols-[3fr_1fr_1fr] gap-14 my-10  mt-40 text-sm'>
+
+        <div>
+          <img className='mb-5 w-40' src={assets.logo} alt="" />
+          <p className='w-full md:w-2/3 text-gray-600 leading-6'>The Tatvamasi Medical Team exemplifies excellence in healthcare. Their dedication and expertise ensure that every patient receives top-notch care. Through teamwork, they strive to improve health outcomes and enhance community well-being. Thank you for your hard work! <b>Cure With Care</b></p>
+          {/* <p className='w-full md:w-2/3 text-gray-600 leading-6'>Experience exceptional care with the Tatvamasi Medical Team! Our dedicated professionals prioritize your health and well-being, offering personalized treatments and compassionate support. Trust us to provide the highest quality medical services in a welcoming environment. Choose Tatvamasi for a healthier tomorrow!. <b>Cure With Care</b></p> */}
+        </div>
+
+        <div>
+          <p className='text-xl font-medium mb-5'>COMPANY</p>
+          <ul className='flex flex-col gap-2 text-gray-600'>
+            <li>Home</li>
+            <li>About us</li>
+            <li>Delivery</li>
+            <li>Privacy policy</li>
+          </ul>
+        </div>
+
+        <div>
+          <p className='text-xl font-medium mb-5'>GET CONNECTED</p>
+          <ul className='flex flex-col gap-2 text-gray-600'>
+            <li>+91 6398414937</li>
+            <li>yjain1049@gmail.com</li>
+          </ul>
+        </div>
+
+      </div>
+
+      <div>
+        <hr />
+        <p className='py-5 text-sm text-center'>Copyright 2024 @ Tatvamasi - All Right Reserved.</p>
+      </div>
+
+    </div>
+  )
+}
+
+export default Footer
